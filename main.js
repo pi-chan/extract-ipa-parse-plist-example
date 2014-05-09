2d4e89c746281725aff2d649c5d0e417a35683ac
xoyip <xoyip@piyox.info>
2014-05-09 16:19:00 +0900

    change css selector and refactoring

--- main.js
+++ main.js
@@ -1,5 +1,8 @@
 (function(obj){
 
+  var bplist = require("bplistParser.js");
+  zip.workerScriptsPath = "./lib/";
+
   var model = (function() {
     var URL = obj.webkitURL || obj.mozURL || obj.URL;
 
@@ -10,81 +13,76 @@
         }, onerror);
       },
       getEntryFile : function(entry, onend, onprogress) {
-        var writer, zipFileEntry;
-
-        function getData() {
-          entry.getData(writer, function(data) {
-            var bplist = require("bplistParser.js");
-
-            var arrayBuffer;
-            var fileReader = new FileReader();
-            fileReader.onload = function() {
-              arrayBuffer = this.result;
-
-              bplist.parseFile(arrayBuffer, function(e, result){
-                if(e) throw e;
-                onend(result);
-              });
-
-            };
-            fileReader.readAsArrayBuffer(data);
-
-          }, onprogress);
-        }
-
-        writer = new zip.BlobWriter();
-        getData();
+        var writer = new zip.BlobWriter();
+        entry.getData(writer, function(data) {
+          onend(data);
+        }, onprogress);
       }
     };
   })();
 
-  var processFile = function(file, htmlList){
+  function processFile(file, appList){
+
+    var onComplete = function(json){
+      var urlTypes = json[0]["CFBundleURLTypes"];
+      if(urlTypes) {        
+
+        var appItem = document.createElement("li");
+        var appName = document.createElement("h3");
+        var schemeList = document.createElement("ul");
+        appItem.appendChild(appName);
+        appItem.appendChild(schemeList);
+        appList.appendChild(appItem);
+        appName.textContent = json[0]["CFBundleDisplayName"] + "(" + json[0]["CFBundleIdentifier"] + ")";
+
+        urlTypes[0]["CFBundleURLSchemes"].forEach(function(urlscheme){
+          var schemeItem = document.createElement("li");
+          schemeItem.textContent = urlscheme;
+          appList.appendChild(schemeItem);
+        });
+      };
+    };
+
     model.getEntries(file, function(entries) {
       entries.forEach(function(entry) {
         if(!entry.filename.match("app/Info.plist")){
           return;
         }
 
-        model.getEntryFile(entry, function(json) {
-          if( json[0]["CFBundleURLTypes"] ) {        
-            var li = document.createElement("li");
-            var h3 = document.createElement("h3");
-            var ul = document.createElement("ul");
-            li.appendChild(h3);
-            li.appendChild(ul);
-            htmlList.appendChild(li);
-            h3.textContent = json[0]["CFBundleDisplayName"] + "(" + json[0]["CFBundleIdentifier"] + ")";
-            json[0]["CFBundleURLTypes"][0]["CFBundleURLSchemes"].forEach(function(urlscheme){
-              var li2 = document.createElement("li");
-              li2.textContent = urlscheme;
-              ul.appendChild(li2);
+        model.getEntryFile(entry, function(data) {
+          var fileReader = new FileReader();
+          fileReader.onload = function() {
+            bplist.parseFile(this.result, function(e, json){
+              if(e) throw e;
+              onComplete(json);
             });
-          }
+          };
+          fileReader.readAsArrayBuffer(data);
         }, null);
       });
     });
   };
 
-  function processFiles(files, htmlList){
+  function processFiles(files, appList){
     var files2 = files.concat();
     var file = files2.shift();
 
-    processFile(file, htmlList);
+    processFile(file, appList);
     if(files2.length > 0){
-      setTimeout(function(){processFiles(files2, htmlList);}, 300);
+      setTimeout(function(){processFiles(files2, appList);}, 300);
     }
   };
 
-  document.getElementById("fileInfo").addEventListener("click", function(){
-    var fileInput = document.getElementById("myFile");
-    var fileList = document.getElementById("file-list");
-    fileList.innerHTML = "";
+  document.getElementById("button-start").addEventListener("click", function(){
+    var fileInput = document.getElementById("file-input");
+    var appList = document.getElementById("app-list");
+    appList.innerHTML = "";
 
     files = [];
     for(var i=0, file=fileInput.files[0]; file = fileInput.files[i]; ++i) {
       files.push(file);
     }
 
-    processFiles(files, fileList);
+    processFiles(files, appList);
   }, true); 
 }(this));
